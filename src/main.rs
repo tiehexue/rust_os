79dea2b6f83d6b7cdec000a8d3608f11d384ec07
汪院 <wangy10@chinatelecom.cn>
2019-05-21 16:38:17 +0800

    integration test.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use rust_os::println;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+  println!("{}", _info);
+  loop {}
 }
 
-static HELLO: &[u8] = b"Hello Rust OS!";
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  rust_os::test_panic_handler(info);
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
 
-    let vga_buffer = 0xb8000 as *mut u8;
+  println!("Hello Rust OS {}", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+  #[cfg(test)]
+  test_main();
 
-    loop {}
+  panic!("Panic now!");
 }
---
+++ tests/panic_handler.rs
@@ -0,0 +1,70 @@
+#![no_std]
+#![no_main]
+#![feature(panic_info_message)]
+
+use core::fmt;
+use core::fmt::Write;
+use core::panic::PanicInfo;
+use rust_os::{QemuExitCode, exit_qemu, serial_print, serial_println};
+
+const MESSAGE: &str = "Example panic message from panic_handler test";
+const PANIC_LINE: u32 = 16; // adjust this when moving the `panic!` call
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+  serial_print!("panic_handler... ");
+  panic!(MESSAGE); // must be in line `PANIC_LINE`
+}
+
+fn check_location(info: &PanicInfo) {
+  let location = info.location().unwrap_or_else(|| fail("no location"));
+
+  if location.file() != file!() {
+    fail(location.file());
+    fail("file name wrong");
+  }
+
+  if location.line() != PANIC_LINE {
+    fail("file line wrong");
+  }
+}
+
+fn check_message(info: &PanicInfo) {
+  let message = info.message().unwrap_or_else(|| fail("no message"));
+  let mut compare_message = CompareMessage { equals: false };
+  write!(&mut compare_message, "{}", message)
+    .unwrap_or_else(|_| fail("write failed"));
+  if !compare_message.equals {
+    fail("message not equal to expected message");
+  }
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  check_message(info);
+  check_location(info);
+
+  serial_println!("[ok]");
+  exit_qemu(QemuExitCode::Success);
+  loop {}
+}
+
+fn fail(error: &str) -> ! {
+  serial_println!("[failed]");
+  serial_println!("{}", error);
+  exit_qemu(QemuExitCode::Failed);
+  loop {}
+}
+
+struct CompareMessage {
+  equals: bool,
+}
+
+impl fmt::Write for CompareMessage {
+  fn write_str(&mut self, s: &str) -> fmt::Result {
+    if s == MESSAGE {
+      self.equals = true;
+    }
+    Ok(())
+  }
+}
